f37026f7ef155ec3110ed1fc2da07bcd2d3b4d4e
immobarok <immobarokali2001@gmail.com>
2025-07-22 22:50:59 +0600

    user creation delete and update from clerk

---
+++ models/User.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  _id: { type: String, require: true },
+  name: { type: String, require: true },
+  email: { type: String, require: true },
+  image: { type: String, require: true }
+})
+
+export const User = mongoose.model('User', userSchema)
---
+++ server.js
@@ -0,0 +1,24 @@
+import { clerkMiddleware } from '@clerk/express'
+import express from 'express';
+import cors from 'cors';
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
+import 'dotenv/config';
+import { connectDB } from './config/database.js';
+
+const app = express();
+const port = 4000;
+
+await connectDB();
+
+app.use(express.json())
+app.use(cors())
+app.use(clerkMiddleware())
+
+app.get('/', (req, res) => {
+  res.send("Server is running")
+})
+app.use('/api/inngest', serve({ client: inngest, functions }))
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`)
+})
